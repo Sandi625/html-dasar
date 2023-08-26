28807a6082bec9be97a76a51a4a62da9d628f067
Sandi625 <sandipermadi625@gmail.com>
2023-08-26 13:43:10 +0800

    first commit

---
+++ assets/katakata.js
@@ -0,0 +1,25 @@
+console.log (document.title)
+
+// document.title = 'fikri gobles';
+
+console.log(document.title)
+
+
+//materi append
+// document.title = 'fikri gobles'
+const body = document.body
+//append untuk body pastinya
+// body.append('Hello World')
+
+const h1 = document.createElement('h1') //ini ibaratkan <h1></h1>
+// h1.textContent = "<marquee>Anjay ini H1 Bro</marquee>" //ini baru  isinya <h1>Anjay ini H1 Bro</h1>
+//inner HTML bisa Membaca <marquee> karna bisa dihitung kita memassukkan element
+const namaSaya = document.createElement('p')
+namaSaya.innerHTML = '<marquee>Halo Halo Halo</marquee>'
+
+const namaKamu = document.createElement('b')
+// namaKamu.innerText = '<marquee>Fikri Gobles</marquee>'
+
+body.append(h1)
+body.append(namaSaya)
+body.append(namaKamu)
