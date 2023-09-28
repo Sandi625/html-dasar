9af219b4278c74ad6afe0c8fa415eddba1bba504
Sandi625 <sandipermadi625@gmail.com>
2023-09-28 14:34:32 +0700

    Penambahan Admin Lte

---
+++ pages/AdminLTE-3.2.0/tugas/newproduct/assets/javascript/katakata.js
@@ -0,0 +1,32 @@
+// console.log (document.title)
+
+// // document.title = 'fikri gobles';
+
+// console.log(document.title)
+
+// // JavaScript to control video playback
+// window.addEventListener('DOMContentLoaded', (event) => {
+//     const video = document.getElementById('bg-video');
+//     video.play(); // Start playing the video automatically
+// });
+
+
+
+// //materi append
+// // document.title = 'fikri gobles'
+// const body = document.body
+// //append untuk body pastinya
+// // body.append('Hello World')
+
+// const h1 = document.createElement('h1') //ini ibaratkan <h1></h1>
+// // h1.textContent = "<marquee>Anjay ini H1 Bro</marquee>" //ini baru  isinya <h1>Anjay ini H1 Bro</h1>
+// //inner HTML bisa Membaca <marquee> karna bisa dihitung kita memassukkan element
+// const namaSaya = document.createElement('p')
+// namaSaya.innerHTML = '<marquee>Halo Halo Halo</marquee>'
+
+// const namaKamu = document.createElement('b')
+// // namaKamu.innerText = '<marquee>Fikri Gobles</marquee>'
+
+// body.append(h1)
+// body.append(namaSaya)
+// body.append(namaKamu)
